26327bbfd25ba2f826bc4207edddd773bd849c4c
ennaouri <ennaouri.mohammed@gmail.com>
2022-07-13 11:03:44 +0100

    fix crossOrigin Tag

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5587331919297301"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
         <script
                   async
                   src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
