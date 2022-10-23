a6562f220ba295bf9fa589029bc2eadafb4f88e8
ennaouri <ennaouri.mohammed@gmail.com>
2022-10-23 02:09:12 +0100

    build

--- components/About.js
+++ components/About.js
@@ -7,8 +7,8 @@ function About() {
         <p>Maths-exams.com: Une plateforme alignée avec votre programme scolaire et adaptée à vos besoins.</p>
         <p>Nous vous souhaitons la bienvenue sur ce site dédié à tous les étudiants, et passionnés de mathématiques.</p>
         <p>Vous pouvez trouver ici des problèmes de mathématiques au lycée et les problèmes de mathématiques les plus populaires des concours.</p>
-        <p>Vous pouvez utiliser maths-exams.com pour préparer aux exams de maths et pour vous aidez à résoudre vos problèmes mathématiques, d'exercices,
-        et pour vous aider à vous préparer aux examens d'admission au grandes ecoles.</p>
+        <p>Vous pouvez utiliser maths-exams.com pour préparer aux exams de maths et pour vous aidez à résoudre vos problèmes mathématiques, d`&apos`exercices,
+        et pour vous aider à vous préparer aux examens d`&apos`admission au grandes ecoles.</p>
         <p>Je suis là pour vous aider à Maths-exams.com !</p>
 
     </div>
