a48a1a43eea76f8ee191c2afd645ce331a3c7d26
ennaouri <ennaouri.mohammed@gmail.com>
2023-01-21 22:18:01 +0100

    deploy devoir4

--- pages/404.js
+++ pages/404.js
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function NoFoundPage() {
   return <>
-        <div class="d-flex align-items-center justify-content-center vh-100">
-        <h1 class="display-1 fw-bold text-white">404</h1>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+        <h1 className="display-1 fw-bold text-white">404</h1>
     </div>
     <Link href="/">
       <a className="link-danger alert-primary bg-dark ">
