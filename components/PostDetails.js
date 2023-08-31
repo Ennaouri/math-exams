21b63a81f39af6d0e78ea904c036c39edef9ff1e
ennaouri <ennaouri.mohammed@gmail.com>
2023-08-31 03:41:15 +0100

    add ads config 7

--- components/PostDetails.js
+++ components/PostDetails.js
@@ -1,14 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button} from "reactstrap"
 import moment from 'moment';
 
 const PostDetail = ({ post }) => {
-  const [date, setDate] = useState(null);
-
-  useEffect(() => {
-    setDate(format(moment(post.createdAt),'MMM DD, YYYY'));
-  }, []);
-
+  
     const getContentFragment = (index, text, obj, type) => {
         let modifiedText = text;
     
@@ -71,7 +66,7 @@ const PostDetail = ({ post }) => {
               <svg xmlns="http://www.w3.org/2000/svg" width="20px" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>{' '}
-              <span className="align-middle">{date}</span>
+              <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
             </div>
           </CardSubtitle>
 {post.content.raw.children.map((typeObj, index) => {
