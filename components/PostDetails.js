18ac8adf2296cd4eaf6e8ac485d47f394785f8ca
ennaouri <ennaouri.mohammed@gmail.com>
2023-08-31 03:30:22 +0100

    add ads config 5

--- components/PostDetails.js
+++ components/PostDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button} from "reactstrap"
 import moment from 'moment';
 
